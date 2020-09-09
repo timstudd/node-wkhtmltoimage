ba72f5a67c333247dd9264d5858bd328bf762a94
Alejandro Romero Herrera <alromh87@gmail.com>
2020-09-09 22:09:36 +0300

    Fix Arbitary Code Execution

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function WKHtmlToImage() {
 			options = {};
 		}
 		
-		var output = options.output;
+		var output = options.output.replace(/'|"|;|&|,|\\|\|/gi, '');
 		delete options.output;
 		
 		var args = [this.command, '--quiet'];
